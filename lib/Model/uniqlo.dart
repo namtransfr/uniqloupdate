a36d56f5b803cbc64674c00027d33d32e8188ae7
namtransfr <pornphunwaree@gmail.com>
2025-12-06 22:49:28 +0700

    UpdateUniqlo

---
+++ lib/Model/uniqlo.dart
@@ -0,0 +1,25 @@
+class Uniqlo {
+  String imgUrl;
+  String imgTitle;
+
+  Uniqlo(this.imgUrl, this.imgTitle);
+
+  static List<Uniqlo> samples =
+  [
+    Uniqlo('assets/images/1.jpg','shirt',),
+    Uniqlo('assets/images/2.jpg','shirt',),
+    Uniqlo('assets/images/3 (2).jpg','shirt',),
+    Uniqlo('assets/images/4.jpg','shirt',),
+    Uniqlo('assets/images/5.jpg','bag',),
+    Uniqlo('assets/images/6.jpg','shirt',),
+    Uniqlo('assets/images/7.jpg','shirt',),
+    Uniqlo('assets/images/8.jpg','shirt',),
+    Uniqlo('assets/images/9.jpg','shirt',),
+    Uniqlo('assets/images/10.jpg','shirt',),
+    Uniqlo('assets/images/11.jpg','shirt',),
+    Uniqlo('assets/images/12.jpg','shirt',),
+    Uniqlo('assets/images/13.jpg','shirt',),
+    Uniqlo('assets/images/14.jpg','shirt',),
+    Uniqlo('assets/images/15.jpg','shirt',),
+  ];
+}
